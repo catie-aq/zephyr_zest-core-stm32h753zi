d9e7d5968b279ce9ff8108081a277569793ff2e8
Clément Laigle <c.laigle@catie.fr>
2024-04-15 15:06:35 +0200

    Update Zest Core STM32H753ZG board configuration

--- boards/arm/zest_core_stm32h753zi/board.cmake
+++ boards/arm/zest_core_stm32h753zi/board.cmake
@@ -1,6 +1,6 @@
 # Copyright (c) 2024 CATIE
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=stm32l4a6rg" "--speed=4000")
+board_runner_args(jlink "--device=stm32h753zi" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
